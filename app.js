7290372524eb3fbf9a5b20a86124cb01306c16f8
Sourav <sourav.sarkar@hih7.com>
2023-06-05 14:28:52 +0530

    Add input and extract data with body parser

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const http = require('http')
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
-app.use((req,res,next)=>{
-    console.log('We in the middlewere')
+app.use(bodyParser.urlencoded({extended:false}))
+app.use('/',(req,res,next)=>{
+    console.log('Always Running.....!!!')
     next()
 })
-app.use((req,res,next)=>{
-    console.log('We in the another middlewere')
-    // res.send('<h1>Hello Froem Express Js!!</h1>')
-    res.send({key:'val1'})
+app.use('/add-product',(req,res,next)=>{
+    res.send(`<html><body><form action ='/product' method='POST'><input type='text' name='title'/><input type='number' name='size'/><button type='submit'>Submit</button></form></body></html>`)
+    
+    
+})
+app.post('/product',(req,res,next)=>{
+    console.log(req.body)
+res.redirect('/')
+})
+app.use('/',(req,res,next)=>{
+    console.log('We in the different  middlewere')
+    res.send('<h1>Welcome To Expres js v-4</h1>')
+    
 })
 
 // const server = http.createServer(app)
